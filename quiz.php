496ede0f6b3a10db4790fddeb52462a119bd6e69
eugenemunemo <j.babybehindmusic@gmail.com>
2019-08-22 01:02:08 -0700

    css
    change of background images

--- quiz.php
+++ quiz.php
@@ -29,11 +29,12 @@
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
     />
-    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <!-- <link href="css/styles.css" rel="stylesheet" type="text/css"> -->
+    <link rel="stylesheet" href="css/styles.css">
   
    <title>My Quiz app</title>
 </head>
-<body id="background">
+<body>
 
 
 
